7585b7136dcbd9d59160fc17f7ddc18104d1435f
loulang <loulangogogo@163.com>
2024-03-20 14:38:07 +0800

    添加redis的配置

--- sentinel-datasource-redis-spring-boot-starter/src/main/java/io/github/loulangogogo/sentinel/datasource/redis/annotation/ConfigCheck.java
+++ sentinel-datasource-redis-spring-boot-starter/src/main/java/io/github/loulangogogo/sentinel/datasource/redis/annotation/ConfigCheck.java
@@ -3,8 +3,10 @@ package io.github.loulangogogo.sentinel.datasource.redis.annotation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.core.env.MissingRequiredPropertiesException;
 
 import javax.annotation.PostConstruct;
+import javax.el.PropertyNotFoundException;
 
 /*********************************************************
  ** 配置检查，确定使用的配置是否已经配置过了
@@ -24,11 +26,11 @@ public class ConfigCheck {
      * @author     :loulan
      * */
     @PostConstruct
-    public void checkRedis() throws Exception {
+    public void checkRedis() {
         if (!env.containsProperty("spring.redis.host") ||
                 !env.containsProperty("spring.redis.port")) {
             // 如果没有这两个配置，那么跑出异常
-            throw new Exception("请引入【spring-boot-starter-data-redis】依赖，并配置redis");
+            throw new PropertyNotFoundException("请引入【spring-boot-starter-data-redis】依赖，并配置redis");
         }
     }
 }
